532429a0a252ac010da8e06488df17173008c155
sunshenghao <1534807383@qq.com>
2025-12-19 13:56:34 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# psychelink_study
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/psychelink_study/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.psychelink_study
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
